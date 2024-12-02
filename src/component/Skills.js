4271c212cda33d4837dd44663582cf9fc6d433c8
ShivanisFSM <shivani15bs@gmail.com>
2024-12-02 12:11:13 +0530

    nextjs

--- src/component/Skills.js
+++ src/component/Skills.js
@@ -6,7 +6,7 @@ const Skills = () => {
       <h2>Technical Skills</h2>
       <ul>
         <li><strong>Languages:</strong>JavaScript, HTML, CSS, TypeScript</li>
-        <li><strong>Libraries/Frameworks:</strong> ReactJS, React Native, Redux, Material UI, Tailwind CSS, Bootstrap</li>
+        <li><strong>Libraries/Frameworks:</strong> ReactJS, React Native, Redux, Next.js, Material UI, Tailwind CSS, Bootstrap</li>
         <li><strong>Tools:</strong> Git, Webpack, Babel, Firebase, Android Studio</li>
         <li><strong>Other:</strong>RESTful APIs, SEO Optimization, Responsive Design, Mobile-First Development, ES6</li>
       </ul>
