212638bb7ab9b4f2f76e5040e11fcf217eae28e0
Shivani Shriramojwar <shivani@Shivanis-MacBook-Air.local>
2025-07-17 00:18:22 +0530

    favicon changes and spacing

--- src/component/Header.js
+++ src/component/Header.js
@@ -17,19 +17,20 @@ const Header = () => (
 
 <div className="resume-header">
       <h1 className="name">SHIVANI SHRIRAMOJWAR</h1>
-      <h2 className="role">
-        Front-End Developer | 3.8 Years’ Experience | React.js | Next.js | TypeScript | SEO & Performance Optimization
-      </h2>
+      <div className="role">
+         Full-stack Developer
+      </div>
       <p className="contact">
    +91 7020955345 | {" "}
   <a href="mailto:shivani15bs@gmail.com">shivani15bs@gmail.com</a> | {" "}
-  <a
-    href="https://www.linkedin.com/in/shivanishriramojwar"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    linkedin.com/in/shivanishriramojwar
-  </a>
+ <a
+  href="https://www.linkedin.com/in/shivanishriramojwar"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  LinkedIn
+</a>
+
 </p>
 
     </div>
--- src/component/Skills.js
+++ src/component/Skills.js
@@ -5,10 +5,10 @@ const Skills = () => {
     <section>
       <h2>Technical Skills</h2>
       <ul>
-        <li><strong>Programming Languages:</strong>JavaScript, TypeScript, HTML, CSS, ES6</li>
+        <li><strong>Programming Languages:</strong> JavaScript, TypeScript </li>
         <li><strong>Libraries/Frameworks:</strong> ReactJS, React Native, Redux, Next.js, Material UI, Tailwind CSS, Bootstrap</li>
         <li><strong>Tools & Platforms:</strong> Git, Webpack, Babel, Firebase, Android Studio, Google Analytics</li>
-        <li><strong>Other Skills:</strong>RESTful APIs, Responsive Design, SEO Optimization, Mobile-First Development, React Query</li>
+        <li><strong>Other Skills:</strong> RESTful APIs, Responsive Design, SEO Optimization, Mobile-First Development, ES6</li>
       </ul>
     </section>
   );
