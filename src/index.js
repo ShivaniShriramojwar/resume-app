abc601b0f4cea019969c34dfa9d2cf9c9b0034ea
Shivani Shriramojwar <shivani@Shivanis-MacBook-Air.local>
2025-07-16 23:08:47 +0530

    home file changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-// src/index.js
+// // src/index.js
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css'; // Your CSS file
+// import { BrowserRouter } from 'react-router-dom';
+// import App from './App';
+
+// ReactDOM.render(
+//   <BrowserRouter basename="/resume-app">  {/* Add basename here */}
+//     <App />
+//   </BrowserRouter>,
+//   document.getElementById('root')
+// );
+
+
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css'; // Your CSS file
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import './index.css'; 
 
-ReactDOM.render(
-  <BrowserRouter basename="/resume-app">  {/* Add basename here */}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter basename="/resume-app">
     <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+  </BrowserRouter>
 );
