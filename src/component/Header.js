c3ca228ae08dbbb92d0614341b283d97a32c525a
Shivani <shivani.bhaskar@nxtgen.com>
2025-07-17 17:09:26 +0530

    responsive changes

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,41 +1,20 @@
-import React from "react";
-
 const Header = () => (
   <header style={{ textAlign: "center" }}>
     <h1 className="responsive-heading">SHIVANI SHRIRAMOJWAR</h1>
     <div className="role">Full-stack Developer</div>
-    <p className="contact-info">
-      +91 7020955345 |{" "}
-      <a href="mailto:shivani15bs@gmail.com">shivani15bs@gmail.com</a> |{" "}
-      <a
-        href="https://www.linkedin.com/in/shivanishriramojwar"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        https://www.linkedin.com/in/shivanishriramojwar
-      </a>
+    <p class="contact-info">
+      <span className="nowrap">
+        +91 7020955345&nbsp;|&nbsp; shivani15bs@gmail.com &nbsp;|&nbsp;
+        <a
+          href="https://www.linkedin.com/in/shivanishriramojwar"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          https://www.linkedin.com/in/shivanishriramojwar
+        </a>
+      </span>
     </p>
   </header>
-
-  //       <div className="resume-header">
-  //       <h1 className="name">SHIVANI SHRIRAMOJWAR</h1>
-  //       <div className="role">
-  //          Full-stack Developer
-  //       </div>
-  //       <p className="contact">
-  //    +91 7020955345 | {" "}
-  //   <a href="mailto:shivani15bs@gmail.com">shivani15bs@gmail.com</a> | {" "}
-  //  <a
-  //   href="https://www.linkedin.com/in/shivanishriramojwar"
-  //   target="_blank"
-  //   rel="noopener noreferrer"
-  // >
-  //   LinkedIn
-  // </a>
-
-  // </p>
-
-  //            </div>
 );
 
 export default Header;
